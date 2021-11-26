eb7d381a8fc80e49f2fd8069e1df31fa5e89f8e2
Fridasaralinnea <Fridasaralinnea@gmail.com>
2021-11-26 15:11:32 +0000

    Fix validation errors

--- app/models.py
+++ app/models.py
@@ -49,18 +49,30 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def follow(self, user):
+        """
+        Follow user
+        """
         if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
+        """
+        Unfollow user
+        """
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
+        """
+        Followed users
+        """
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
+        """
+        Posts for followed and user
+        """
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
